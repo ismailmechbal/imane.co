d12b5ae2abdcac07179b00f25293930585cf332e
Ismail Mechbal <ismail.mechbal@gmail.com>
2021-09-25 07:48:48 +0200

    Move to svelte

---
+++ src/routes/index.svelte
@@ -0,0 +1,38 @@
+<div class="container">
+  <h1>Imane Ben Salah</h1>
+  <h2>PhD in Molecular Phytopathology</h2>
+  <div class="subtitle">Experience</div>
+  <ul>
+    <li>
+      <p><b>PhD Researcher | Dec 2012 – Dec 2018</b></p>
+      <p>Cadi Ayyad University - INRA - Marrakech, Morocco</p>
+    </li>
+    <li>
+      <p><b>Research Training | Jun 2012 – Aug 2012</b></p>
+      <p>National Food Research Institute - Tsukuba, Japan</p>
+      <p>Preparation of Oil-in-Water emulsions.</p>
+    </li>
+    <li>
+      <p><b>Research Training | Feb 2012 – May 2012</b></p>
+      <p>University of Tsukuba - Tennondai, Japan</p>
+      <p>Formulation of glycerol-lipids or fatty acids-based emulsions using micro-channel technology.</p>
+    </li>
+    <li>
+      <p><b>Intern - MSc Traineeship | May 2011</b></p>
+      <p>COPAG dairy factory, UHT department - Taroudannt, Morocco</p>
+    </li>
+    <li>
+      <p><b>Intern - BASc Traineeship | Jul 2010</b></p>
+      <p>Social Development Agency - Rabat, Morocco</p>
+    </li>
+  </ul>
+  <div class="subtitle">Publications</div>
+  <ul>
+    <li>
+      <h2>Seaweed polysaccharides as bio-elicitors of natural defenses in olive trees against verticillium wilt of olive</h2>
+      <a href="https://www.tandfonline.com/doi/full/10.1080/17429145.2018.1471528" title="Seaweed polysaccharides as bio-elicitors of natural defenses in olive trees against verticillium wilt of olive" rel="nofollow">Journal of Plant Interactions</a>
+    </li>
+  </ul>
+  <div class="subtitle">CV</div>
+  <p>See my profile on <a href="https://www.linkedin.com/in/imane-ben-salah-37495470/" rel="nofollow">LinkedIn</a>.</p>
+</div>
